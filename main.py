1719cfe4ffd98745715eba10fa8f93100481bfbb
Patrick Dugan <dugan.pat@gmail.com>
2013-05-15 09:18:20 -0700

    Pat dugan personal portfolio
    initial checkin for portfolio and blog template

---
+++ main.py
@@ -0,0 +1,40 @@
+import cgi
+import wsgiref.handlers
+import os
+import re
+import os
+import urllib
+import json
+import logging
+
+from datetime import datetime
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.db import GqlQuery
+from google.appengine.api import urlfetch
+                    
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'themes/templates/index.html')
+    template_values = None
+    self.response.out.write(template.render(path, template_values))
+    
+class ProjectPage(webapp.RequestHandler):
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'themes/templates/project_view.html')
+    template_values = None
+    self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication([
+  ('/project', ProjectPage),
+  ('/', MainPage)
+], debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
