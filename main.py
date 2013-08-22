78c47c61eca65c664a4e72ee88b1b366d2ed09f7
Patrick Dugan <dugan.pat@gmail.com>
2013-08-22 13:11:15 -0700

    portfolio changes
    switched to jinja2, webapp2, and added portfolio comments

--- main.py
+++ main.py
@@ -6,47 +6,88 @@ import os
 import urllib
 import json
 import logging
+import jinja2
+import webapp2
 
 from datetime import datetime
-from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 from google.appengine.api import users
-from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.db import GqlQuery
 from google.appengine.api import urlfetch
-                    
-class MainPage(webapp.RequestHandler):
-  def get(self):
-    path = os.path.join(os.path.dirname(__file__), 'themes/templates/index.html')
-    template_values = None
-    self.response.out.write(template.render(path, template_values))
-    
-class ProjectPage(webapp.RequestHandler):
-  def get(self):
-    path = os.path.join(os.path.dirname(__file__), 'themes/templates/project_view.html')
-    template_values = None
-    self.response.out.write(template.render(path, template_values))
-    
-class BlogPage(webapp.RequestHandler):
-  def get(self):
-    path = os.path.join(os.path.dirname(__file__), 'themes/templates/blog.html')
-    template_values = None
-    self.response.out.write(template.render(path, template_values))
-    
-class AboutPage(webapp.RequestHandler):
-  def get(self):
-    path = os.path.join(os.path.dirname(__file__), 'themes/templates/about.html')
-    template_values = None
-    self.response.out.write(template.render(path, template_values))
-    
-class BooksPage(webapp.RequestHandler):
-  def get(self):
-    path = os.path.join(os.path.dirname(__file__), 'themes/templates/books.html')
-    template_values = None
-    self.response.out.write(template.render(path, template_values))
 
-application = webapp.WSGIApplication([
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+# Global variable passed into empty templates  
+emptyList = []
+
+class ProjectEntry(db.Model):
+  projectId = db.StringProperty()
+  projectName = db.StringProperty()
+  projectSummary = db.TextProperty()
+  projectDates = db.StringProperty()
+  projectCompany = db.StringProperty()
+                    
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    template_values = {
+      'emptyList': emptyList
+    }
+    path = jinja_environment.get_template('themes/templates/index.html')
+    self.response.out.write(path.render(template_values))
+    
+class ProjectPage(webapp2.RequestHandler):
+  def get(self):
+    projectName = None   
+    if self.request.get('projectName'):
+      projectName = self.request.get('projectName')
+    
+    projectData = GqlQuery(
+      "SELECT * FROM ProjectEntry WHERE projectId ='" + 
+      str(projectName) + "'"
+    )
+    
+    for project in projectData:
+      projectName = project.projectName
+      projectSummary = project.projectSummary
+      projectDates = project.projectDates
+      projectCompany = project.projectCompany
+      
+    template_values = {
+      'projectName': projectName,
+      'projectSummary': projectSummary,
+      'projectDates': projectDates,
+      'projectCompany': projectCompany
+    }
+    path = jinja_environment.get_template('themes/templates/project_view.html')
+    self.response.out.write(path.render(template_values))
+    
+class BlogPage(webapp2.RequestHandler):
+  def get(self):
+    template_values = {
+      'emptyList': emptyList
+    }
+    path = jinja_environment.get_template('themes/templates/blog.html')
+    self.response.out.write(path.render(template_values))
+    
+class AboutPage(webapp2.RequestHandler):
+  def get(self):
+    template_values = {
+      'emptyList': emptyList
+    }
+    path = jinja_environment.get_template('themes/templates/about.html')
+    self.response.out.write(path.render(template_values))
+    
+class BooksPage(webapp2.RequestHandler):
+  def get(self):
+    template_values = {
+      'emptyList': emptyList
+    }
+    path = jinja_environment.get_template('themes/templates/books.html')
+    self.response.out.write(path.render(template_values))
+
+application = webapp2.WSGIApplication([
   ('/project', ProjectPage),
   ('/blog', BlogPage),
   ('/portfolio', MainPage),
